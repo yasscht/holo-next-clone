aaba6391b0c89e1efcb39247a12910fc7909dd06
yassine.chtiaa@etu.uae.ac.ma <yassine.chtiaa@etu.uae.ac.ma>
2022-04-22 23:47:18 +0000

    finishing up the header component

--- components/Header.jsx
+++ components/Header.jsx
@@ -2,17 +2,27 @@ import Image from "next/image";
 import HeaderItem from "./kit/HeaderItem";
 import {
   BadgeCheckIcon,
-  CollectionItem,
+  CollectionIcon,
   HomeIcon,
   LightningBoltIcon,
   SearchIcon,
   UserIcon,
 } from "@heroicons/react/outline";
 const Header = () => {
+  const HeaderItems = [
+    { title: "HOME", Icon: HomeIcon },
+    { title: "TRENDING", Icon: LightningBoltIcon },
+    { title: "VERIFIED", Icon: BadgeCheckIcon },
+    { title: "COLLECTIONS", Icon: CollectionIcon },
+    { title: "SEARCH", Icon: SearchIcon },
+    { title: "ACCOUNT", Icon: UserIcon },
+  ];
   return (
-    <div>
-      <div>
-        <HeaderItem title="HOME" Icon={HomeIcon} />
+    <div className="flex items-center  flex-col sm:flex-row justify-between pr-4">
+      <div className="flex items-center justify-evenly flex-grow max-w-2xl p-6">
+        {HeaderItems.map(({ title, Icon }) => (
+          <HeaderItem title={title} Icon={Icon} />
+        ))}
       </div>
       <div>
         <Image src="https://links.papareact.com/ua6" width={200} height={100} />
--- components/kit/HeaderItem.jsx
+++ components/kit/HeaderItem.jsx
@@ -2,7 +2,7 @@ const HeaderItem = ({ title, Icon }) => {
   return (
     <div className="flex flex-col items-center cursor-pointer w-12 sm:w-20 hover:text-white group">
       <Icon className="w-8 group-hover:animate-bounce" />
-      <span className="tracking-widest opacity-100 group-hover:opacity-100">
+      <span className="tracking-widest opacity-0 group-hover:opacity-100">
         {title}
       </span>
     </div>
