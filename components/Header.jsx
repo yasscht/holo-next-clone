7084e1c5625ce7ca16f457e5cdbedf0e727484e8
yassine.chtiaa@etu.uae.ac.ma <yassine.chtiaa@etu.uae.ac.ma>
2022-04-22 23:35:38 +0000

    implementing the headerItem component

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,9 +1,22 @@
 import Image from "next/image";
-
+import HeaderItem from "./kit/HeaderItem";
+import {
+  BadgeCheckIcon,
+  CollectionItem,
+  HomeIcon,
+  LightningBoltIcon,
+  SearchIcon,
+  UserIcon,
+} from "@heroicons/react/outline";
 const Header = () => {
   return (
     <div>
-      <Image src="https://links.papareact.com/ua6" width={200} height={100} />
+      <div>
+        <HeaderItem title="HOME" Icon={HomeIcon} />
+      </div>
+      <div>
+        <Image src="https://links.papareact.com/ua6" width={200} height={100} />
+      </div>
     </div>
   );
 };
