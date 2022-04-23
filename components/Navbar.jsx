be5c28012fefbe6c151636a4b10a4dc51c5040b6
yassine.chtiaa@etu.uae.ac.ma <yassine.chtiaa@etu.uae.ac.ma>
2022-04-23 01:48:43 +0000

    UI improovement

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -2,15 +2,22 @@ import request from "../utils/request";
 
 const Navbar = () => {
   return (
-    <nav className="flex items-center scrollbar-hide justify-between space-x-10 overflow-auto max-w-5xl sm:max-w-full whitespace-nowrap p-7 text-base font-semibold ">
-      {Object.entries(request).map(([key, { title, url }]) => (
-        <h3
-          key={key}
-          className="cursor-pointer transition duration-100 transform hover:scale-110 hover:text-white active:text-red-500"
-        >
-          {title}
-        </h3>
-      ))}
+    <nav className="relative ">
+      <div
+        className="flex items-center scrollbar-hide justify-between space-x-10 overflow-auto
+          max-w-5xl sm:max-w-full whitespace-nowrap p-7 text-base font-semibold "
+      >
+        {Object.entries(request).map(([key, { title, url }]) => (
+          <h3
+            key={key}
+            className="cursor-pointer transition duration-100 transform hover:scale-110 hover:text-white active:text-red-500"
+          >
+            {title}
+          </h3>
+        ))}
+      </div>
+
+      <div className="absolute top-0 right-0 bg-gradient-to-l from-[#06202A] h-14 w-1/12" />
     </nav>
   );
 };
