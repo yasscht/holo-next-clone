4782dc7e2c39af3a85aa3feb2337bca27fc9dde2
yassine.chtiaa@etu.uae.ac.ma <yassine.chtiaa@etu.uae.ac.ma>
2022-04-23 14:26:05 +0000

    add the 4K breakpoint

--- components/Movies.jsx
+++ components/Movies.jsx
@@ -2,7 +2,7 @@ import MovieCard from "./kit/MovieCard";
 
 const Movies = ({ results }) => {
   return (
-    <div className=" px-5 my-10 sm:grid md:grid-cols-2 xl:grid-cols-3">
+    <div className=" px-5 my-10 sm:grid md:grid-cols-2 xl:grid-cols-3 3xl:flex flex-wrap justify-center">
       {results.map((result) => (
         <MovieCard key={result.id} result={result} />
       ))}
